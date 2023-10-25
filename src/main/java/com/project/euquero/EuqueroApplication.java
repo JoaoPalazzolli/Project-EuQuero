cd9c7ea6ce852a5e06142687a4e37e2b9002d416
João Palazzolli <joaopedropalazzolli@gmail.com>
2023-10-24 23:30:09 -0300

    Iniciando Projeto

---
+++ src/main/java/com/project/euquero/EuqueroApplication.java
@@ -0,0 +1,13 @@
+package com.project.euquero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuqueroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuqueroApplication.class, args);
+	}
+
+}
