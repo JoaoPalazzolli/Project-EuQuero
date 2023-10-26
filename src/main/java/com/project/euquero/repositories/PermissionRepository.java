5e6b78a94ed87705cc517a1f490818dd32d58d79
João Palazzolli <joaopedropalazzolli@gmail.com>
2023-10-26 17:05:47 -0300

    Criado um metodo para selecionar a permission pelo atributo "descricao"

--- src/main/java/com/project/euquero/repositories/PermissionRepository.java
+++ src/main/java/com/project/euquero/repositories/PermissionRepository.java
@@ -3,5 +3,9 @@ package com.project.euquero.repositories;
 import com.project.euquero.models.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+
+    Optional<Permission> findByDescricao(String descricao);
 }
