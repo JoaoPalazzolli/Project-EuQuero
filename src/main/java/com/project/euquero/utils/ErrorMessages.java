e98748e269ea09269b86a0a0aead29294f271a9d
João Palazzolli <joaopedropalazzolli@gmail.com>
2023-11-19 15:15:16 -0300

    Adicionado uma mensagem de erro para arquivos null

--- src/main/java/com/project/euquero/utils/ErrorMessages.java
+++ src/main/java/com/project/euquero/utils/ErrorMessages.java
@@ -10,6 +10,7 @@ public class ErrorMessages {
     public static final String ADDRESS_CONFLICT = "This Address is Already Registered";
     public static final String CONTENT_NOT_BLANK = "This Content cannot be Blank";
     public static final String CONTENT_NOT_NULL = "This Content cannot be Null";
+    public static final String FILE_NOT_NULL = "File cannot be Null";
     public static final String INVALID_FIELD = "Invalid Field";
     public static final String EMAIL_OR_PHONENUMBER_CONFLICT = "this Email or PhoneNumber already exists";
     public static final String BAD_CREDENTIALS = "Invalid email/password supplied!";
