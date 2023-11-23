b0c27c5f7c00a24b1075d16b940c056ba0b01f64
João Palazzolli <joaopedropalazzolli@gmail.com>
2023-11-23 17:44:39 -0300

    Adicionado método para pegar organizacao que tenha um determinado userId e organizacaoId

--- src/main/java/com/project/euquero/repositories/OrganizacaoRepository.java
+++ src/main/java/com/project/euquero/repositories/OrganizacaoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrganizacaoRepository extends JpaRepository<Organizacao, Long> {
 
@@ -16,10 +17,20 @@ public interface OrganizacaoRepository extends JpaRepository<Organizacao, Long> 
             """)
     Boolean existsCNPJ(@Param("cnpj") String cnpj);
 
+    Boolean existsByNomeAndDescricao(String nome, String descricao);
+
     @Query(value = """
             select * from organizacao
             inner join user_organizacao on user_organizacao.organizacao_id = organizacao.id
             where user_organizacao.user_id = :userId
             """, nativeQuery = true)
     List<Organizacao> findOrganizacaoByUserId(@Param("userId") Long UserId);
+
+    @Query(value = """
+            select * from organizacao
+            inner join user_organizacao on user_organizacao.organizacao_id = organizacao.id
+            where user_organizacao.user_id = :userId and user_organizacao.organizacao_id = :organizacaoId
+            """, nativeQuery = true)
+    Optional<Organizacao> findByOrganizacaoIdAndUserId(@Param("organizacaoId") Long organizacaoId, @Param("userId") Long userId);
+
 }
